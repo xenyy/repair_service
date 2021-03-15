4ac2c400c3f0a545bd1357a47ac1fadaef08ffce
xenyy <xenydev@gmail.com>
2021-03-15 15:38:25 +0100

    aded features

---
+++ lib/models/data_models/static_data/drawer_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DrawerTile {
+  final IconData icon;
+  final String text;
+  final String route;
+
+  DrawerTile(
+    this.icon,
+    this.text,
+    this.route,
+  );
+}
---
+++ lib/models/data_models/static_data/settings_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SettsTile {
+  final IconData icon;
+  final String text;
+  final String route;
+
+  SettsTile(
+    this.icon,
+    this.text,
+    this.route,
+  );
+}
